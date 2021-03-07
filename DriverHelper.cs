caa06c20f78f744d74206d0b860f7d839b4ba9a6
souilmiaymen <aymen.souilmi.91@gmail.com>
2021-03-07 15:19:53 -0500

    Upload files

---
+++ DriverHelper.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpecFlowNUnitDemo
+{
+    public class DriverHelper
+    {
+        public IWebDriver Driver
+        {
+            get;
+            set;
+        }
+    }
+}
